778ec2b62e1c944012ded41675994ce703920188
bokob <bmc0520@naver.com>
2023-04-01 21:04:49 +0900

    ejs화 하기 전

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require("express"); // 라이브러리 첨부
 const app = express(); // 객체 생성
 app.use(express.urlencoded({ extended: true }));
-
 const MongoClient = require("mongodb").MongoClient;
+app.set("view engine", "ejs");
+app.use("/public", express.static("public"));
+
 var db;
 var url = require("./key.json").url;
 MongoClient.connect(url, { useUnifiedTopology: true }, function (에러, client) {
   if (에러) return console.log(에러);
   db = client.db("todoapp");
 
-  db.collection("post").insertOne(
-    { 이름: "John", _id: 100 },
-    function (에러, 결과) {
-      console.log("저장완료");
-    }
-  );
+  // db.collection("post").insertOne(
+  //   { 이름: "John", _id: 100 },
+  //   function (에러, 결과) {
+  //     console.log("저장완료");
+  //   }
+  // );
 
   app.listen(8080, function () {
     console.log("listening on 8080");
@@ -37,13 +39,55 @@ app.get("/write", function (요청, 응답) {
   응답.sendFile(__dirname + "/write.html");
 });
 
-app.post("/add", function (req, res) {
-  res.send("전송완료");
-
-  db.collection("post").insertOne(
-    { 제목: req.body.title, 날짜: req.body.date },
+app.post("/add", function (요청, 응답) {
+  db.collection("counter").findOne(
+    { name: "게시물갯수" },
     function (에러, 결과) {
-      console.log("저장완료");
+      var 총게시물갯수 = 결과.totalPost;
+
+      db.collection("post").insertOne(
+        { _id: 총게시물갯수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date },
+        function (에러, 결과) {
+          db.collection("counter").updateOne(
+            { name: "게시물갯수" },
+            { $inc: { totalPost: 1 } },
+            function (에러, 결과) {
+              if (에러) {
+                return console.log(에러);
+              }
+              응답.send("전송완료");
+            }
+          );
+        }
+      );
+    }
+  );
+});
+
+app.get("/list", function (req, res) {
+  db.collection("post")
+    .find()
+    .toArray(function (에러, 결과) {
+      console.log(결과);
+      res.render("list.ejs", { posts: 결과 });
+    });
+});
+
+app.delete("/delete", function (req, res) {
+  console.log(req.body);
+  req.body._id = parseInt(req.body._id);
+  db.collection("post").deleteOne(req.body, function (에러, 결과) {
+    console.log("삭제완료");
+    res.status(200).send({ message: "성공했습니다" });
+  });
+});
+
+app.get("/detail/:id", function (req, res) {
+  db.collection("post").findOne(
+    { _id: parseInt(req.params.id) },
+    function (에러, 결과) {
+      console.log(결과);
+      res.render("detail.ejs", { data: 결과 });
     }
   );
 });
